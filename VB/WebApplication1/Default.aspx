5c90d001acb64a4fdc7471ba0ea67f5c58a0ad0f
DevExpressExampleBot <fakeEmail>
2026-01-29 16:54:31 +0400

    Source auto update [skip ci]

--- VB/WebApplication1/Default.aspx
+++ VB/WebApplication1/Default.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="Home Page" Language="C#" AutoEventWireup="true"
+<%@ Page Title="Home Page" Language="VB" AutoEventWireup="true"
     CodeBehind="Default.aspx.vb" Inherits="WebApplication1._Default" %>
 
 <%@ Register Assembly="DevExpress.Web.v24.2, Version=24.2.13.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
@@ -14,33 +14,27 @@
 </head>
 <body>
     <form id="Form1" runat="server">
+        <asp:AccessDataSource ID="AccessDataSource1" runat="server" DataFile="~/App_Data/nwind.mdb"
+            SelectCommand="SELECT [CategoryID], [CategoryName], [Description] FROM [Categories]">
+        </asp:AccessDataSource>
+        <dx:ASPxButton ID="ASPxButton1" runat="server" OnClick="ASPxButton1_Click" Text="Export to PDF" />
 
-    <asp:AccessDataSource ID="AccessDataSource1" runat="server" DataFile="~/App_Data/nwind.mdb"
-        SelectCommand="SELECT [CategoryID], [CategoryName], [Description] FROM [Categories]">
-    </asp:AccessDataSource>
-    <dx:ASPxButton ID="ASPxButton1" runat="server" OnClick="ASPxButton1_Click" 
-        Text="Export to PDF">
-    </dx:ASPxButton>
-    <dx:ASPxGridViewExporter ID="ASPxGridViewExporter1" runat="server" 
-        GridViewID="ASPxGridView1">
-    </dx:ASPxGridViewExporter>
-    <dx:ASPxGridView ID="ASPxGridView1" runat="server" AutoGenerateColumns="False" DataSourceID="AccessDataSource1"
-        KeyFieldName="CategoryID" OnCustomSummaryCalculate="ASPxGridView1_CustomSummaryCalculate"
-        OnHtmlFooterCellPrepared="ASPxGridView1_HtmlFooterCellPrepared">
-        <Columns>
-                    <dx:GridViewDataTextColumn FieldName="CategoryID"  />
-                    <dx:GridViewDataTextColumn FieldName="CategoryName" />
-                      <dx:GridViewDataTextColumn FieldName="Description" />
-                    <dx:GridViewDataTextColumn FieldName="CategoryName" />
-                      <dx:GridViewDataTextColumn FieldName="CategoryID" />
-                    <dx:GridViewDataTextColumn FieldName="CategoryName" />
-                      <dx:GridViewDataTextColumn FieldName="Description"/>
-                    <dx:GridViewDataTextColumn FieldName="CategoryName" />
-                    <dx:GridViewDataTextColumn FieldName="Description" />
-                    <dx:GridViewDataTextColumn FieldName="CategoryName"/>
-                      <dx:GridViewDataTextColumn FieldName="CategoryID"/>
-                </columns>
-    </dx:ASPxGridView>
-        </form>
+        <dx:ASPxGridView ID="ASPxGridView1" runat="server" AutoGenerateColumns="False" DataSourceID="AccessDataSource1"
+            KeyFieldName="CategoryID" >
+            <Columns>
+                <dx:GridViewDataTextColumn FieldName="CategoryID"  />
+                <dx:GridViewDataTextColumn FieldName="CategoryName" />
+                <dx:GridViewDataTextColumn FieldName="Description" />
+                <dx:GridViewDataTextColumn FieldName="CategoryName" />
+                <dx:GridViewDataTextColumn FieldName="CategoryID" />
+                <dx:GridViewDataTextColumn FieldName="CategoryName" />
+                <dx:GridViewDataTextColumn FieldName="Description"/>
+                <dx:GridViewDataTextColumn FieldName="CategoryName" />
+                <dx:GridViewDataTextColumn FieldName="Description" />
+                <dx:GridViewDataTextColumn FieldName="CategoryName"/>
+                <dx:GridViewDataTextColumn FieldName="CategoryID"/>
+            </Columns>
+        </dx:ASPxGridView>
+    </form>
 </body>
 </html>
--- VB/WebApplication1/Default.aspx.vb
+++ VB/WebApplication1/Default.aspx.vb
@@ -1,8 +1,5 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
+Imports System
 Imports System.Web
-Imports System.Web.UI
 Imports System.Web.UI.WebControls
 Imports System.IO
 Imports DevExpress.XtraPrinting
@@ -10,53 +7,39 @@ Imports System.Net.Mime
 Imports DevExpress.XtraPrintingLinks
 
 Namespace WebApplication1
-	Partial Public Class _Default
-		Inherits System.Web.UI.Page
 
-		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+    Public Partial Class _Default
+        Inherits UI.Page
 
-		End Sub
+        Protected Sub ASPxButton1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Using ms As MemoryStream = New MemoryStream()
+                Dim pcl As PrintableComponentLinkBase = New PrintableComponentLinkBase(New PrintingSystemBase())
+                pcl.Component = ASPxGridView1
+                pcl.Margins.Right = 50
+                pcl.Margins.Left = pcl.Margins.Right
+                pcl.Landscape = True
+                pcl.CreateDocument(False)
+                pcl.PrintingSystemBase.Document.AutoFitToPagesWidth = 1
+                pcl.ExportToPdf(ms)
+                Call WriteResponse(Response, ms.ToArray(), DispositionTypeNames.Inline.ToString())
+            End Using
+        End Sub
 
-		Protected Sub ASPxButton1_Click(ByVal sender As Object, ByVal e As EventArgs)
-			Using ms As New MemoryStream()
-				Dim pcl As New PrintableComponentLinkBase(New PrintingSystemBase())
-				pcl.Component = ASPxGridViewExporter1
-'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
-'ORIGINAL LINE: pcl.Margins.Left = pcl.Margins.Right = 50;
-				pcl.Margins.Right = 50
-				pcl.Margins.Left = pcl.Margins.Right
-				pcl.Landscape = True
-				pcl.CreateDocument(False)
-				pcl.PrintingSystemBase.Document.AutoFitToPagesWidth = 1
-				pcl.ExportToPdf(ms)
-				WriteResponse(Me.Response, ms.ToArray(), System.Net.Mime.DispositionTypeNames.Inline.ToString())
-			End Using
-		End Sub
-		Public Shared Sub WriteResponse(ByVal response As HttpResponse, ByVal filearray() As Byte, ByVal type As String)
-			response.ClearContent()
-			response.Buffer = True
-			response.Cache.SetCacheability(HttpCacheability.Private)
-			response.ContentType = "application/pdf"
-			Dim contentDisposition As New ContentDisposition()
-			contentDisposition.FileName = "test.pdf"
-			contentDisposition.DispositionType = type
-			response.AddHeader("Content-Disposition", contentDisposition.ToString())
-			response.BinaryWrite(filearray)
-			HttpContext.Current.ApplicationInstance.CompleteRequest()
-			Try
-				response.End()
-			Catch e1 As System.Threading.ThreadAbortException
-			End Try
-
-		End Sub
-
-		Protected Sub ASPxGridView1_CustomSummaryCalculate(ByVal sender As Object, ByVal e As DevExpress.Data.CustomSummaryEventArgs)
-
-		End Sub
-
-		Protected Sub ASPxGridView1_HtmlFooterCellPrepared(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxGridViewTableFooterCellEventArgs)
-
-		End Sub
-
-	End Class
+        Public Shared Sub WriteResponse(ByVal response As HttpResponse, ByVal filearray As Byte(), ByVal type As String)
+            response.ClearContent()
+            response.Buffer = True
+            response.Cache.SetCacheability(HttpCacheability.Private)
+            response.ContentType = "application/pdf"
+            Dim contentDisposition As ContentDisposition = New ContentDisposition()
+            contentDisposition.FileName = "test.pdf"
+            contentDisposition.DispositionType = type
+            response.AddHeader("Content-Disposition", contentDisposition.ToString())
+            response.BinaryWrite(filearray)
+            Call HttpContext.Current.ApplicationInstance.CompleteRequest()
+            Try
+                response.End()
+            Catch __unusedThreadAbortException1__ As Threading.ThreadAbortException
+            End Try
+        End Sub
+    End Class
 End Namespace
